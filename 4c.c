3834ca8d28a1478721b62a846cf3994ed28cecad
geetas07 <geetasharanappa8801@gmail.com>
2024-11-07 13:48:02 +0530

    Add files via upload

---
+++ 4c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   int a=2,b=5,t;
+   t=0;
+   scanf("%d%i",&a,&b);
+   printf("before swap %d and %i",a,b);
+   a=a;
+
+   printf("");
+
+   return 0;
+}
---
+++ a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int Sod(int);
+int main()
+{
+    int n=0;
+    scanf("%d",&n);
+    printf(" sum of digit no %d of %d");
+
+
+
+}
+int Sod(int n);
+{
+    return(n<=0) return 0;
+    return(n==1) return 1;
+    return(n%10)+sod(n/10);
+}
